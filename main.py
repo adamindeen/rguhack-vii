aa034b689ed2f7b6bd8fe447923f4df083e59ce7
Adam Weir <adam.weir@fennex.net>
2022-03-26 19:47:53 +0000

    19:47

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+# Imports
+import string
 import plotly.express as px
+import pandas as pd
 import numpy as np
-df = px.data.gapminder()
-fig = px.scatter_3d(df, x='year', y='continent', z='pop', size='gdpPercap', color='lifeExp',
-                    hover_data=['country'])
-fig.update_layout(scene_zaxis_type="log")
-fig.show()
+
+# The board
+# a = np.zeros((3, 3, 3))
+# m = np.array([[[ 0.,  0.,  0.,  0.],
+#         [ 0.,  0.,  0.,  0.],
+#         [ 0.,  0.,  0.,  0.]],
+
+#        [[ 0.,  0.,  0.,  0.],
+#         [ 0.,  0.,  0.,  0.],
+#         [ 0.,  0.,  0.,  0.]]])
+
+def lazer(df: pd.DataFrame, axis: string, axisx,axisy):
+    for i in range(len(df.index)):
+        
+        print(df[i])
+
+
+df = pd.DataFrame(columns=['player','x','y','z'])
+df.add(1,0,0,0)
+df.add(1,1,0,0)
+print(df)
+
+# fig = px.scatter_3d(df, color='player', x='x', y='y', z='z', hover_data=['player'])
+# fig.update_layout(scene_zaxis_type="log")
+# fig.show()
+lazer(df,'x',1,1)
