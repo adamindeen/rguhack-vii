2c2461c765512a696b7b19f150349ffd622c1bad
Adam Weir <adam.weir@fennex.net>
2022-03-26 20:55:46 +0000

    shit

--- main.py
+++ main.py
@@ -2,30 +2,20 @@
 import string
 import plotly.express as px
 import pandas as pd
-import numpy as np
-
-# The board
-# a = np.zeros((3, 3, 3))
-# m = np.array([[[ 0.,  0.,  0.,  0.],
-#         [ 0.,  0.,  0.,  0.],
-#         [ 0.,  0.,  0.,  0.]],
-
-#        [[ 0.,  0.,  0.,  0.],
-#         [ 0.,  0.,  0.,  0.],
-#         [ 0.,  0.,  0.,  0.]]])
 
 def lazer(df: pd.DataFrame, axis: string, axisx,axisy):
     for i in range(len(df.index)):
-        
-        print(df[i])
+			  print(df.iloc(i))
 
+data = [
+	{'player': 1, 'x': 0, 'y': 0, 'z': 0},
+	{'player': 1, 'x': 1, 'y': 0, 'z': 0}
+]
+df = pd.DataFrame(columns=['player','x','y','z'], data=data)
 
-df = pd.DataFrame(columns=['player','x','y','z'])
-df.add(1,0,0,0)
-df.add(1,1,0,0)
 print(df)
 
-# fig = px.scatter_3d(df, color='player', x='x', y='y', z='z', hover_data=['player'])
-# fig.update_layout(scene_zaxis_type="log")
-# fig.show()
+fig = px.scatter_3d(df, color='player', x='x', y='y', z='z', hover_data=['player'])
+fig.update_layout(scene_zaxis_type="log")
+fig.show()
 lazer(df,'x',1,1)
